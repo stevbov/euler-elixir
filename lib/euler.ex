5fe2d3975c70fbad7427b4dcb1abb017f7b23b13
Steven Bovatsek <steven@sbov.net>
2020-09-04 20:55:15 -0700

    Solve problem 1: sum multiples of 3 or 5

--- lib/euler.ex
+++ lib/euler.ex
@@ -1,2 +1,7 @@
 defmodule Euler do
+  def problem1 do
+    1..999
+    |> Stream.filter(fn num -> rem(num, 3) == 0 || rem(num, 5) == 0 end)
+    |> Enum.reduce(0, &+/2)
+  end
 end
--- test/euler_test.exs
+++ test/euler_test.exs
@@ -1,3 +1,7 @@
 defmodule EulerTest do
   use ExUnit.Case
+
+  test "problem 1" do
+    assert Euler.problem1 == 233168
+  end
 end

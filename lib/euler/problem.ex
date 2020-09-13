afdc60e5f6419d90155d429edfd3422ab737a319
Steven Bovatsek <steven@sbov.net>
2020-09-13 14:13:30 -0700

    Move code that ensures modules are loaded to task

--- lib/euler/problem.ex
+++ lib/euler/problem.ex
@@ -7,14 +7,8 @@ defmodule Euler.Problem do
   Gets all problems
   """
   def all_problems() do
-    :code.all_available()
-    |> Enum.map(fn {module_name, _, _} -> module_name end)
-    |> Enum.map(&to_string/1)
-    |> Enum.filter(&String.starts_with?(&1, "Elixir.Euler.Problem"))
-    |> Enum.map(&String.to_atom/1)
-    # need to make sure the code is actually loaded - it won't always be
-    |> Enum.map(&Code.ensure_loaded/1)
-    |> Enum.map(fn {:module, module} -> module end)
+    :code.all_loaded()
+    |> Enum.map(fn {module, _} -> module end)
     # make sure the module implements the Euler.Problem behavior
     |> Enum.filter(fn module ->
       Enum.member?(module.module_info(:attributes), {:behaviour, [Euler.Problem]})
@@ -41,7 +35,7 @@ defmodule Euler.Problem do
       !match?({_, ""}, Integer.parse(problem_number)) ->
         {:error, "#{problem_number} is not a number"}
 
-      match?({:error, _}, Code.ensure_loaded(module)) ->
+      match?(:not_loaded, :code.module_status(module)) ->
         {:error, "No solution for problem ##{problem_number}"}
 
       !Enum.member?(module.module_info(:attributes), {:behaviour, [Euler.Problem]}) ->
